4838401e4cf435d78cbeed9265301d0bc1adbe6d
gaoyanbao <494189180@qq.com>
2020-01-12 21:26:38 +0800

    备份文档sql

--- src/main/java/com/xyl/a_helloworld/HelloWorld.java
+++ src/main/java/com/xyl/a_helloworld/HelloWorld.java
@@ -1,7 +1,5 @@
 package com.xyl.a_helloworld;
 
-import java.util.List;
-
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
@@ -9,8 +7,30 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 
+import java.util.List;
+
 public class HelloWorld {
-	
+
+	/**
+	 * -- 部署对象和流程定义表
+	 * select * from act_re_deployment #部署对象表
+	 * select * from act_re_procdef #流程定义表
+	 * select * from act_ge_bytearray #资源文件表
+	 * select * from act_ge_property #主键生成策略表
+	 * -- 流程实例，执行对象，任务
+	 * select * from act_ru_execution #正在执行的执行对象表
+	 * select * from act_hi_procinst #流程实例的历史表
+	 * select * from act_ru_task #正在执行的任务表
+	 * select * from act_hi_taskinst #任务历史表(只有节点是usertask的时候，该表中存在数据)
+	 * select * from act_hi_actinst #所有活动节点的历史表
+	 * -- 流程变量
+	 * select * from act_ru_variable #正在执行的流程变量表
+	 * select * from act_hi_varinst #历史的流程变量表
+	 * <p>
+	 * select * from act_ru_identitylink #任务办理人表(个人任务,组任务)
+	 * <p>
+	 * select * from act_hi_identitylink #历史任务办理人表(个人任务,组任务)
+	 */
 	ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 
 	/**部署流程定义*/
